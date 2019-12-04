62c1001d6389f2d1f3067b5c4af260623e3f686a
Bryant Eisenbach <fubuloubu@gmail.com>
2019-12-04 13:48:50 -0500

    feat: Added JSON serde provider

--- substratum/providers/base.py
+++ substratum/providers/base.py
@@ -1,3 +1,5 @@
+import json
+import itertools
 from typing import (
     Any,
 )
@@ -6,8 +8,31 @@ from substratum.types import (
     RPCEndpoint,
     RPCResponse,
 )
+from substratum.utils import (
+    to_bytes,
+    to_text,
+)
 
 
 class BaseProvider:
     def make_request(self, method: RPCEndpoint, params: Any) -> RPCResponse:
         raise NotImplementedError("Providers must implement this method")
+
+
+class JSONBaseProvider(BaseProvider):
+    def __init__(self) -> None:
+        self.request_counter = itertools.count()
+
+    def decode_rpc_response(self, raw_response: bytes) -> RPCResponse:
+        text_response = to_text(raw_response)
+        return json.loads(text_response)
+
+    def encode_rpc_request(self, method: RPCEndpoint, params: Any) -> bytes:
+        rpc_dict = {
+            "jsonrpc": "2.0",
+            "method": method,
+            "params": params or [],
+            "id": next(self.request_counter),
+        }
+        encoded = json.dumps(rpc_dict)
+        return to_bytes(text=encoded)
