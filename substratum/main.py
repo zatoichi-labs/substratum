2fffd6dd3fde0cd994ad51f8519e95009013f09a
Bryant Eisenbach <fubuloubu@gmail.com>
2019-12-04 16:06:25 -0500

    feat: Abstracted module registration so that any set can be used

--- substratum/main.py
+++ substratum/main.py
@@ -1,14 +1,29 @@
+from typing import (
+    Dict,
+)
+
 from substratum.providers import (
     BaseProvider,
 )
 from substratum.api import (
     Chain,
+    Module,
     System,
 )
 
 
+def get_default_modules() -> Dict[str, Module]:
+    return {
+        "system": System,
+        "chain": Chain,
+    }
+
+
 class Substratum:
-    def __init__(self, provider: BaseProvider) -> None:
+    def __init__(self, provider: BaseProvider, modules: Dict[str, Module]=None) -> None:
         self.provider = provider
-        self.chain = Chain(self.provider)
-        self.system = System(self.provider)
+        if modules is None:
+            modules = get_default_modules()
+
+        for namespace, module in modules.items():
+            setattr(self, namespace, module(self.provider))
