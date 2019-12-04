b719496452bb060712b3c69a4a2badaf985aaaa8
Bryant Eisenbach <fubuloubu@gmail.com>
2019-12-04 14:12:34 -0500

    feat: Added user agent helper, to_dict, and request manager

--- substratum/utils.py
+++ substratum/utils.py
@@ -1,6 +1,35 @@
+from typing import (
+    Any,
+)
+import requests
+
 from eth_utils import (
+    to_dict,
     to_hex,
     to_int,
     to_bytes,
     to_text,
 )
+
+from substratum.types import (
+    URI,
+)
+
+def construct_user_agent(class_name):
+    from substratum import __version__ as version
+
+    user_agent = 'Substratum/{version}/{class_name}'.format(
+        version=version,
+        class_name=class_name,
+    )
+    return user_agent
+
+
+def make_post_request(endpoint_uri: URI, data: bytes, *args: Any, **kwargs: Any) -> bytes:
+    kwargs.setdefault('timeout', 10)
+    session = requests.Session()
+    # https://github.com/python/mypy/issues/2582
+    response = session.post(endpoint_uri, data=data, *args, **kwargs)  # type: ignore
+    response.raise_for_status()
+
+    return response.content
