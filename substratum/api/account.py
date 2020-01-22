009d1640e954905a153431aff1c3a992212fd299
Bryant Eisenbach <fubuloubu@gmail.com>
2020-01-21 19:20:12 -0500

    refactor: subkey changed how Keyring access works
    correctly outputs AccountId now

--- substratum/api/account.py
+++ substratum/api/account.py
@@ -12,10 +12,18 @@ from substratum.providers import (
 from substratum.types import (
     AccountId,
 )
+from substratum.utils import (
+    SS58,
+    blake2b,
+)
 
 from .module import (
     Module,
 )
+
+
+def pair_to_account_id(pair: KeyringPair) -> AccountId:
+    return SS58.encode(pair.public if pair.key_type != 'secp256k1' else blake2b(pair.public))
 
 
 class Account(Module):
@@ -27,14 +35,17 @@ class Account(Module):
         Generate a new local account and store it
         :returns: AccountId of newly generated account
         """
-        pair = self._keyring.generate()
-        return AccountId(pair.public)
+        self._keyring.generate()
+        pair = self._keyring.pairs[-1]
+        return pair_to_account_id(pair)
 
     def add_from_uri(self, uri, key_type=None) -> AccountId:
         if not key_type:
             key_type = self._keyring.default_key_type
-        a = self._keyring.add_from_uri(uri, key_type=key_type)
-        return AccountId(a)
+        self._keyring.add_from_uri(uri, key_type=key_type)
+        a = self._keyring.pairs[-1]
+        return pair_to_account_id(a)
 
     def getAccounts(self) -> List[AccountId]:
-        return [AccountId(a.public) for a in self._keyring.pairs]
+        # TODO: Remote accounts (in addition to local ones)
+        return [pair_to_account_id(a) for a in self._keyring.pairs]
